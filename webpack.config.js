f8c86b0769abd772f8e037882e438ae505c38b83
dhavalsinhzala <dhaval.zala@live.com>
2016-09-19 18:03:21 +0530

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: './js/app.jsx',
+  output: {
+    path: __dirname + '/js',
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          cacheDirectory: true,
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+}
